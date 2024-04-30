c9bceeb597cb340b8a71c1c6274299a30862e25f
UmarRamzan <94057389+UmarRamzan@users.noreply.github.com>
2024-04-30 14:58:56 +0500

    Update +page.svelte

--- web-app/src/routes/Scoreboard/+page.svelte
+++ web-app/src/routes/Scoreboard/+page.svelte
@@ -10,6 +10,13 @@
     import { onMount } from 'svelte';
     import { goto } from '$app/navigation';
 
+    let loading = true;
+    onMount(() => {
+      setTimeout(() => {
+      loading = false;
+    }, 3000); // hides the spinner after 5 seconds
+    })
+
     let gray1 = "bg-gray-900"
     let gray2 = "bg-gray-800"
     let white = "bg-white"
@@ -177,102 +184,120 @@
     let outerContainer 
 
   </script>
+
+{#if loading}
+<main>
+  <div class="spinner"></div>
+  <p class="text-2xl font-bold text-black my-5">Match In Progress</p>
+</main>
+
+{:else}
   <!-- outer container, nests scoreboard -->
   <div class="min-h-screen w-full bg-gray-200 py-8 px-8">
-  <div class="bg-white p-4 min-h-screen text-black rounded">
-    <div class="flex justify-between items-center mb-4 text-lg">
-      <div>
-        <div class="flex items-center">
-          <img src={PakFlag} alt="Pakistan" class="w-10 h-8 mr-2"/>
-          <p class="font-bold ">{matchInfo.teams.home.score} ({matchInfo.teams.home.overs})</p>
+    <div class="bg-white p-4 min-h-screen text-black rounded">
+      <div class="flex justify-between items-center mb-4 text-lg">
+        <div>
+          <div class="flex items-center">
+            <img src={PakFlag} alt="Pakistan" class="w-10 h-8 mr-2"/>
+            <p class="font-bold ">{matchInfo.teams.home.score} ({matchInfo.teams.home.overs})</p>
+          </div>
+        </div>
+        <div>
+          <div class="flex items-center">
+            <img src={NewsFlag} alt="New Zealand" class="w-10 h-8 mr-2"/>
+            <p>{matchInfo.teams.away.score} ({matchInfo.teams.away.overs})</p>
+          </div>
         </div>
       </div>
-      <div>
-        <div class="flex items-center">
-          <img src={NewsFlag} alt="New Zealand" class="w-10 h-8 mr-2"/>
-          <p>{matchInfo.teams.away.score} ({matchInfo.teams.away.overs})</p>
-        </div>
+      <div class="flex flex-col mb-4 items-center text-lg">
+        <p>{matchInfo.result}</p>
+        <p>{matchInfo.seriesStatus}</p>
       </div>
-    </div>
-    <div class="flex flex-col mb-4 items-center text-lg">
-      <p>{matchInfo.result}</p>
-      <p>{matchInfo.seriesStatus}</p>
+    
+      <div class="mb-4">
+        <!--buttons for switching team players-->
+        <div class="flex justify-center gap-4 mb-6">
+          <button
+              class={`px-4 py-2 rounded transition-colors ${currentTeam === 'Lahore Qalandars' ? 'bg-red-700 text-white' : 'bg-gray-800 text-gray-400'}`}
+              on:click={() => switchTeam('Lahore Qalandars')}>
+              Lahore Qalandars
+          </button>
+          <button
+              class={`px-4 py-2 rounded transition-colors ${currentTeam === 'Karachi Kings' ? 'bg-red-700 text-white' : 'bg-gray-800 text-gray-400'}`}
+              on:click={() => switchTeam('Karachi Kings')}>
+              Karachi Kings
+          </button>
+      </div>
+        <table class="min-w-full">
+          <thead>
+            <tr>
+              <th class="text-left">Batting</th>
+              <th>R</th>
+              <th>B</th>
+              <th>4s</th>
+              <th>6s</th>
+              <th>S/R</th>
+            </tr>
+          </thead>
+          <tbody>
+            {#each players as player}
+            <tr class="border-b border-gray-700">
+              <td class="py-2">
+                <div class="font-semibold">{player.name}</div>
+                <div class="text-xs text-gray-400">{player.status}</div>
+              </td>
+              <td>{player.runs}</td>
+              <td>{player.balls}</td>
+              <td>{player.fours}</td>
+              <td>{player.sixes}</td>
+              <td>{player.strikeRate}</td>
+            </tr>
+            {/each}
+          </tbody>
+        </table>
+  
+        <table class="min-w-full mt-8">
+          <thead>
+            <tr>
+              <th class="text-left">Bowling</th>
+              <th>O</th>
+              <th>R</th>
+              <th>W</th>
+              <th>Econ</th>
+            </tr>
+          </thead>
+          <tbody>
+            {#each players_bowling as player}
+            <tr class="border-b border-gray-700">
+              <td class="py-2">
+                <div class="font-semibold">{player.name}</div>
+              </td>
+              <td>{player.overs}</td>
+              <td>{player.runs}</td>
+              <td>{player.wickets}</td>
+              <td>{player.economy}</td>
+            </tr>
+            {/each}
+          </tbody>
+      </div>
+      <div class="next-button flex justify-center my-5 p-2 rounded bg-red-700 text-white" on:click={()=>{goto('/results')}}>
+        <button class=" text-lg">Next</button>
+      </div>
     </div>
   
-    <div class="mb-4">
-      <!--buttons for switching team players-->
-      <div class="flex justify-center gap-4 mb-6">
-        <button
-            class={`px-4 py-2 rounded transition-colors ${currentTeam === 'Lahore Qalandars' ? 'bg-red-700 text-white' : 'bg-gray-800 text-gray-400'}`}
-            on:click={() => switchTeam('Lahore Qalandars')}>
-            Lahore Qalandars
-        </button>
-        <button
-            class={`px-4 py-2 rounded transition-colors ${currentTeam === 'Karachi Kings' ? 'bg-red-700 text-white' : 'bg-gray-800 text-gray-400'}`}
-            on:click={() => switchTeam('Karachi Kings')}>
-            Karachi Kings
-        </button>
     </div>
-      <table class="min-w-full">
-        <thead>
-          <tr>
-            <th class="text-left">Batting</th>
-            <th>R</th>
-            <th>B</th>
-            <th>4s</th>
-            <th>6s</th>
-            <th>S/R</th>
-          </tr>
-        </thead>
-        <tbody>
-          {#each players as player}
-          <tr class="border-b border-gray-700">
-            <td class="py-2">
-              <div class="font-semibold">{player.name}</div>
-              <div class="text-xs text-gray-400">{player.status}</div>
-            </td>
-            <td>{player.runs}</td>
-            <td>{player.balls}</td>
-            <td>{player.fours}</td>
-            <td>{player.sixes}</td>
-            <td>{player.strikeRate}</td>
-          </tr>
-          {/each}
-        </tbody>
-      </table>
+{/if}
 
-      <table class="min-w-full mt-8">
-        <thead>
-          <tr>
-            <th class="text-left">Bowling</th>
-            <th>O</th>
-            <th>R</th>
-            <th>W</th>
-            <th>Econ</th>
-          </tr>
-        </thead>
-        <tbody>
-          {#each players_bowling as player}
-          <tr class="border-b border-gray-700">
-            <td class="py-2">
-              <div class="font-semibold">{player.name}</div>
-            </td>
-            <td>{player.overs}</td>
-            <td>{player.runs}</td>
-            <td>{player.wickets}</td>
-            <td>{player.economy}</td>
-          </tr>
-          {/each}
-        </tbody>
-    </div>
-    <div class="next-button flex justify-center my-5 p-2 rounded bg-red-700 text-white" on:click={()=>{goto('/results')}}>
-      <button class=" text-lg">Next</button>
-    </div>
-  </div>
-
-  </div>
   
   <style>
+    main {
+      height: 100vh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      text-align: center;
+    }
     th, td {
       padding: 0.5rem;
       text-align: center;
@@ -288,6 +313,24 @@
     .next-button:hover {
       cursor: pointer;
     }
+
+    .spinner {
+    border: 4px solid rgba(0, 0, 0, 0.1);
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    border-left-color: rgb(255, 0, 0);
+    animation: spin 1s infinite linear;
+  }
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
 
   </style>
   
